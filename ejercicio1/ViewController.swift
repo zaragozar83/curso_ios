6dc5c10c4b3ce1113d8474f872328c0a413f3fad
zaragozar83 <zaragozar83@gmail.com>
2016-02-01 23:15:05 -0600

    Mensaje del comité

--- ejercicio1/ViewController.swift
+++ ejercicio1/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Comentario 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
