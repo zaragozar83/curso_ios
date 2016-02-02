5569b6ecdbac6d479f82ca351e90df374e888c35
zaragozar83 <zaragozar83@gmail.com>
2016-02-01 23:10:36 -0600

    Initial Commit

---
+++ ejercicio1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ejercicio1
+//
+//  Created by Ricardo Zaragoza Solís on 01/02/16.
+//  Copyright © 2016 Ricardo Zaragoza Solís. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
